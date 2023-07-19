dc43959dca53fa153678896f1e09f8b97ef7ec99
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-07-19 21:25:53 +0900

    ++

--- demo222delete/src/main/java/com/example/demo/Demo222deleteApplication.java
+++ demo222delete/src/main/java/com/example/demo/Demo222deleteApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Demo222deleteApplication {
 
 	public static void main(String[] args) {
-		System.out.println("asd");
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Demo222deleteApplication.class, args);
 	}
 
 }
